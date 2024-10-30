6f6305562568a0b6fe8c5a5d89f5ca0df0384f51
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-30 14:04:34 +0300

    fix: fix uri

--- src/features/auth/model/services/gitHubAuth.ts
+++ src/features/auth/model/services/gitHubAuth.ts
@@ -1,7 +1,8 @@
 import { BASE_URL } from '@/src/shared/constants/api'
 
 export function gitHubAuth() {
-  const REDIRECT_URL = `${BASE_URL}/auth/github`
+  //const REDIRECT_URL = `${BASE_URL}/github`
+  const REDIRECT_URL = `https://picthentic.online/github`
 
   const url = `https://inctagram.work/api/v1/auth/github/login?redirect_uri=${REDIRECT_URL}`
 
